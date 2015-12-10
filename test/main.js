ef80b642721addbf4d344e6f852edd8dabdcde5b
Bretton Wade <Bretton.Wade@gmail.com>
2015-12-10 16:48:23 -0500

    a few tweaks to add teh package and a test file

---
+++ test/main.js
@@ -0,0 +1,7 @@
+#! /usr/local/bin/node
+
+var getSync = require("..");
+
+process.stderr.write ("BEFORE\n");
+getSync ("http://namespace-include.azurewebsites.net/package/abc.js", "abc.js");
+process.stderr.write ("AFTER\n");
