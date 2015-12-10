79330391c6b70349f3f7ee8a8f5d141f5f0d7405
Bretton Wade <Bretton.Wade@gmail.com>
2015-12-10 16:52:32 -0500

    saving changes while I move to linux for testing

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 #! /usr/local/bin/node
 
-var getSync = require("..");
+var getSync = require ("..");
 
 process.stderr.write ("BEFORE\n");
 getSync ("http://namespace-include.azurewebsites.net/package/abc.js", "abc.js");
